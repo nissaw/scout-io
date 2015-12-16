6709499b23653692142b31cc73a8f64e6875d2fa
Jaysig <jasontsigmon@gmail.com>
2015-12-16 16:49:00 -0500

    login icon and homepage text

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,38 +1,6 @@
 'use strict';
 
-// class LoginController {
-//   //start-non-standard
-//   user = {};
-//   errors = {};
-//   submitted = false;
-//   //end-non-standard
 
-//   constructor(Auth, $state) {
-//     this.Auth = Auth;
-//     this.$state = $state;
-//   }
-
-//   login(form) {
-//     this.submitted = true;
-
-//     if (form.$valid) {
-//       this.Auth.login({
-//         email: this.user.email,
-//         password: this.user.password
-//       })
-//       .then(() => {
-//         // Logged in, redirect to home
-//         this.$state.go('main');
-//       })
-//       .catch(err => {
-//         this.errors.other = err.message;
-//       });
-//     }
-//   }
-// }
-
-// angular.module('ScoutIOApp')
-//   .controller('LoginController', LoginController);
 
 
 angular.module('ScoutIOApp.login', ['ngMaterial'])
@@ -40,7 +8,7 @@ angular.module('ScoutIOApp.login', ['ngMaterial'])
   .directive('login', [function() {
     return {
       restrict: 'EA',
-      template: '<a ng-hide="isLoggedIn" href="#" ng-click="openLogin()">Login</a>',
+      template: '<a ng-hide="isLoggedIn" href="#" ng-click="openLogin()"></a>',
       controller: 'LoginDirectiveCtrl'
     };
   }])
