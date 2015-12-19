02bbde0fe9f6cbbe5460d066591254b393e43882
Jaysig <jasontsigmon@gmail.com>
2015-12-18 19:43:21 -0500

    basic project view
    day one of jsTree done
    
    working on project view
    
    projects pull from database

---
+++ client/app/projects/projects.controller.js
@@ -0,0 +1,135 @@
+angular.module('ScoutIOApp')
+    .controller('ProjectsController', ProjectsController);
+
+function ProjectsController($log, $http, $timeout) {
+    var vm = this;
+
+    var newId = 1;
+    vm.ignoreChanges = false;
+    vm.newNode = {};
+    vm.originalData = [{
+        id: 'ajson1',
+        parent: '#',
+        text: 'Simple root node',
+        type: 'cloud',
+        state: {
+            opened: true
+        }
+    }, {
+        id: 'ajson2',
+        parent: '#',
+        text: 'Root node 2',
+        state: {
+            opened: true
+        }
+    }, {
+        id: 'ajson3',
+        parent: 'ajson2',
+        text: 'Child 1',
+        state: {
+            opened: true
+        }
+    }, {
+        id: 'ajson4',
+        parent: 'ajson2',
+        text: 'Child 2',
+        state: {
+            opened: true
+        }
+    }];
+    vm.treeData = [];
+    angular.copy(vm.originalData, vm.treeData);
+    vm.treeConfig = {
+        core: {
+            multiple: false,
+            animation: true,
+            error: function(error) {
+                $log.error(': error from js tree - ' + angular.toJson(error));
+            },
+            check_callback: true,
+            worker: true
+        },
+        types: {
+            default: {
+                icon: 'glyphicon glyphicon-flash'
+            },
+            star: {
+                icon: 'glyphicon glyphicon-star'
+            },
+            cloud: {
+                icon: 'glyphicon glyphicon-cloud'
+            }
+        },
+        version: 1,
+        plugins: ['types', 'checkbox']
+    };
+
+
+    vm.reCreateTree = function() {
+        vm.ignoreChanges = true;
+        angular.copy(this.originalData, this.treeData);
+        vm.treeConfig.version++;
+    };
+
+    vm.simulateAsyncData = function() {
+        vm.promise = $timeout(function() {
+            vm.treeData.push({
+                id: (newId++).toString(),
+                parent: vm.treeData[0].id,
+                text: 'Async Loaded'
+            });
+        }, 3000);
+    };
+
+    vm.getProjects = function() {
+        $http.get('/api/projects/').then(function(response) {
+
+            //comes back as an array of object
+            //need to use append project's name as an object's text key
+            response.data.forEach(project => {
+                console.log(project, 'project name');
+                vm.treeData.push({
+                    id: (newId++).toString(),
+                    parent: '#',
+                    state: {
+                        opened: true
+                    },
+                    text: project.name
+                });
+                console.log(vm.treeData);
+            });
+            vm.originalData = vm.treeData;
+        });
+    };
+
+    vm.addNewNode = function() {
+        vm.treeData.push({
+            id: (newId++).toString(),
+            parent: vm.newNode.parent,
+            text: vm.newNode.text
+        });
+    };
+
+    this.setNodeType = function() {
+        var item = _.findWhere(this.treeData, {
+            id: this.selectedNode
+        });
+        item.type = this.newType;
+        // toaster.pop('success', 'Node Type Changed', 'Changed the type of node ' + this.selectedNode);
+    };
+
+    this.readyCB = function() {
+        $timeout(function() {
+            vm.ignoreChanges = false;
+            // toaster.pop('success', 'JS Tree Ready', 'Js Tree issued the ready event')
+        });
+    };
+
+    this.createCB = function(e, item) {
+        // $timeout(function() {toaster.pop('success', 'Node Added', 'Added new node with the text ' + item.node.text)});
+    };
+
+    this.applyModelChanges = function() {
+        return !vm.ignoreChanges;
+    };
+};
