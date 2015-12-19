46a58bcfb87a2f4fd6c5a72e465d3dfae994b45e
Paul Bhorjee <paul@bhorjee.com>
2015-12-19 13:36:51 -0600

    some style changes and mapping on popup up to know

--- client/components/modal/photoDialogController.js
+++ client/components/modal/photoDialogController.js
@@ -1,37 +1,94 @@
+
 angular.module('ScoutIOApp')
-  .controller('PhotoDialogCtrl', ['$mdDialog', '$scope', '$state', function ($mdDialog, $scope, $state) {
+  .factory('photoDialogService', function () {
+    var origin = '';
+
+    return {
+      saveOrigin:function (data) {
+        origin = data;
+        console.log(data);
+      },
+      getOrigin:function () {
+        return origin;
+      }
+    };
+  });
+
+angular.module('ScoutIOApp')
+  .controller('PhotoDialogCtrl', ['$mdDialog', '$scope', '$rootScope', '$state', function ($mdDialog, $rootScope, $scope, $state, NgMap) {
     $scope.showDialog = function (e, photo) {
       $scope.photo = photo;
+      $scope.photoDialogFlexWidth = "80";
+      $scope.origin = '';
+      //$rootScope.origin = "";
+      //var origin = photoDialogService.getOrigin();
+
+      //function getOrigin()
+      //{
+      //  origin = photoDialogService.getOrigin;
+      //}
+
+      if ($scope.photo.height_m > $scope.photo.width_m) {
+        $scope.photoDialogFlexWidth = "60";
+      }
+
+      $scope.placeChanged = function () {
+        var place = this.getPlace();
+        $scope.origin = place.geometry.location.lat() + "," + place.geometry.location.lng();
+        //photoDialogService.setOrigin(place.geometry.location.lat() + "," + place.geometry.location.lng());
+      };
 
       $mdDialog.show({
         targetEvent: e,
-        locals: {photo: $scope.photo},
+        locals: {
+          photo: $scope.photo,
+          photoDialogFlexWidth: $scope.photoDialogFlexWidth,
+          placeChanged: $scope.placeChanged,
+          origin: $scope.origin
+          //origin: $rootScope.origin
+        },
         controller: DialogController,
         controllerAs: 'dialog',
         bindToController: true,
         templateUrl: 'components/modal/photoDialog.html',
         parent: angular.element(document.body),
         clickOutsideToClose: true
-      }).then(function () {
-        var map = new google.maps.Map(document.getElementById('smallmap'), {
-
-          center: {lat: dialog.photo.latitude, lng: dialog.photo.longitude},
-          zoom: 8
-        });
-        console.log("map", map);
-      })
+      });
     };
 
-    function DialogController($scope, $mdDialog) {
+    function DialogController($scope, $rootScope, $mdDialog, NgMap) {
       $scope.hide = function () {
         $mdDialog.hide();
       };
+
       $scope.cancel = function () {
         $mdDialog.cancel();
       };
+
       $scope.answer = function (answer) {
         $mdDialog.hide(answer);
       };
-    }
 
+      NgMap.getMap({id: 'photomap'}).then(function (map) {
+        $scope.map = map;
+
+        //some hackery to make the map appear in this dialog and then recenter it as
+        // resizing pushed the center downa nd to the right for some reason
+        var center = $scope.map.getCenter();
+        google.maps.event.trigger($scope.map, 'resize');
+        $scope.map.setCenter(center);
+      });
+
+      //var setMarker = function() {
+      //  NgMap.getMap({id: 'photomap'}).then(function (map) {
+      //    $scope.map = map;
+      //
+      //    var myLatlng = new google.maps.LatLng(photo.latitude, photo.longitude);
+      //    var marker = new google.maps.Marker({position: myLatlng, animation: "DROP"});
+      //    marker.setMap($scope.map);
+      //  });
+      //};
+      //
+      //setTimeout(setMarker, 500);
+    }
   }]);
