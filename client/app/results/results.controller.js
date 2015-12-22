0c1648e83cfc2fab0a4790ad2068ce3f15d8b04c
nissaw <nissa.wollum@gmail.com>
2015-12-22 08:08:18 -0800

    common ground on the $rootScope issue

--- client/app/results/results.controller.js
+++ client/app/results/results.controller.js
@@ -9,14 +9,14 @@ function ResultsController($state, $http, NgMap, Search, $rootScope) {
     keywords: '',
     setting: {
       indoor: false,
-      outdoor: false
+      outdoor: false   
     },
     radius: 5,
     startDate: '',
     endDate: '',
     tag: 'all keywords'
   };
-
+ 
   results.name = "Scout IQ";
   results.map = null;
   results.mapStyle = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
@@ -46,6 +46,7 @@ function ResultsController($state, $http, NgMap, Search, $rootScope) {
         }
 
         setMarkers();
+
       })
   };
 
