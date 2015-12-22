23d980f78418eca4f1c90626378196054dc211bb
nissaw <nissa.wollum@gmail.com>
2015-12-22 12:15:17 -0800

    fixed directive names

--- client/components/save-dialog/save-dialog.directive.js
+++ client/components/save-dialog/save-dialog.directive.js
@@ -1,13 +1,13 @@
 angular.module('ScoutIOApp')
-  .directive('SaveDialog', [function(){
+  .directive('saveDialog', [function(){
     return {
       restrict: 'EA',
       templateUrl: '/save-dialog.html',
       controlller: 'SaveDialogController',
-      scope = {
+      scope: {
         // needs access to ... photo currently in the dialog
       },
-      link = function($scope, element, attrs){
+      link: function($scope, element, attrs){
         // when saved is successfull close dialog and show header across photoDialog
       }
     };
--- client/components/save-dialog/save-dialogController.js
+++ client/components/save-dialog/save-dialogController.js
@@ -1,15 +1,15 @@
 angular.module('ScoutIOApp')
   .controller('SaveDialogController', function(Auth, Save){
-//this is controller for the saveDialog 
 
-function DialogController($scope, $mdDialog) {
-  $scope.hide = function() {
-    $mdDialog.hide();
-  };
-  $scope.cancel = function() {
-    $mdDialog.cancel();
-  };
-  $scope.answer = function(answer) {
-    $mdDialog.hide(answer);
-  };
-}
+    function DialogController($scope, $mdDialog) {
+      $scope.hide = function() {
+        $mdDialog.hide();
+      };
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+      $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+      };
+    };
+  });
