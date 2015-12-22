f9b48f14010a64c6726ca6ee35fc0447e7336ff0
nissaw <nissa.wollum@gmail.com>
2015-12-22 15:29:13 -0800

    folder-factory and project-factory methods stubbed out

---
+++ client/app/services/folder-factory.js
@@ -0,0 +1,142 @@
+
+angular.module('ScoutIOApp')
+  .factory('Folder', function ($http, Project) {
+  
+    var folders = [];
+    var folderPaths = [];
+    var projects = [];
+
+
+
+//GET ALL USER FOLDERS
+// gets all projects then all folders from each project
+  var getUserFolders = function(){
+    Project.getUserProjects()
+      .then(function(response){
+        projects = response.projects;
+        _.each(projects, function(project){
+          Project.getProjectFolders(project.id)
+            .then(function(response){
+              folders.push(response)
+            })
+            .catch(function(error){
+              console.log(error, "in getProjectFolders")
+            })
+        });
+      })
+      .catch(function(error){
+        console.log(error, "in getUserProjects")
+      })
+  };
+
+// GET FOLDER BY ID
+  var getFolderById = function(folder){
+      return $http({
+        method: 'GET',
+        url: '/api/folders/' + folder.id
+      })
+        .then(function(response){
+          console.log(response);
+          return response; 
+        })
+        .catch(function(error){
+          console.log(error, "in catch: folder-factory")
+        });
+    };
+
+//GET FOLDER LINKS
+    var getFolderLinks = function(folder) {
+      return $http({
+        method: 'GET',
+        url: '/api/folders/' + folder.id + '/links'
+      })
+        .then(function(response){
+          console.log(response);
+          return response;      
+        })
+        .catch(function(error){
+          console.log(error, "in catch: folder-factory")
+        });
+    };
+//GET FOLDER ASSETS
+    var getFolderAssets = function(folder) {
+      return $http({
+        method: 'GET',
+        url: '/api/folders/' + folder.id + '/assets'
+      })
+        .then(function(response){
+          console.log(response);
+          return response;      
+        })
+        .catch(function(error){
+          console.log(error, "in catch: folder-factory")
+        });
+    };
+
+
+//CREATE A FOLDER
+  var createFolder = function(folder){
+    return $http({
+      method: 'POST',
+      url: '/api/folders',
+      data: folder
+    })
+      .then(function(response){
+        console.log('folder was created');
+        var message = 'Your Folder Has Been Successfully Saved';
+        return message;  
+      })
+      .catch(function(error){
+        console.log(error, "in catch: folder-factory")
+      });
+  };
+
+//EDIT A FOLDER 
+  var editFolder = function(folder){
+    return $http({
+      method: 'PUT',
+      url: '/api/folders/' + folder.id,
+      data: folder
+    })
+      .then(function(response){
+        console.log('folder was updated');
+        var message = 'Your Folder Has Been Successfully Updated';
+        return message;
+      })
+      .catch(function(error){
+        console.log(error, "in catch: folder-factory")
+      });
+  };
+
+//DELETE A FOLDER 
+  var deleteFolder = function(folder){
+    return $http({
+      method: 'DELETE',
+      url: '/api/folders/' + folder.id
+    })
+      .then(function(response){
+        console.log('folder was deleted');
+        var message = 'Your Folder Has Been Successfully Deleted';
+        return message;
+      })
+      .catch(function(error){
+        console.log(error, "in catch: folder-factory")
+      });
+  };
+
+
+
+    return {
+      getFolders: function(){return folders;},
+      getFolderPaths: function(){return folderPaths;},
+      getProjects: function(){return projects;},
+      getUserFolders: getUserFolders,
+      getFolderById: getFolderById,
+      getFolderLinks: getFolderLinks,
+      getFolderAssets: getFolderAssets,
+      createFolder: createFolder,
+      editFolder: editFolder,
+      deleteFolder: deleteFolder
+    };
+
+  });
